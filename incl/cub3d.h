ed5263e459aa06b456e3e840b1369c0c2da2c703
Marvin <leibenguth.marvin@web.de>
2024-05-20 11:23:25 +0200

    Made a to-do list and created the headerfile

--- incl/cub3d.h
+++ incl/cub3d.h
@@ -1,19 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: marvinleibenguth <marvinleibenguth@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/20 09:10:14 by marvinleibe       #+#    #+#             */
-/*   Updated: 2024/05/20 11:18:40 by marvinleibe      ###   ########.fr       */
+/*   Created: 2024/05/20 11:14:31 by marvinleibe       #+#    #+#             */
+/*   Updated: 2024/05/20 11:21:53 by marvinleibe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#if !defined(CUB3D_H)
+#define CUB3D_H
 
-// static void error(void)
-// {
-// 	puts(mlx_strerror(mlx_errno));
-// 	exit(EXIT_FAILURE);
-// }
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "MLX42/MLX42.h"
+#define WIDTH 512
+#define HEIGHT 512
+
+#endif // CUB3D_H
