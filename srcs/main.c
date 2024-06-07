b68cc2126f3e1f8c687e2a09e6111e5018518e84
mleibeng <mleibeng@student.42.fr>
2024-06-07 08:01:52 +0200

    Created a print function for fkeitel for debugging info

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mleibeng <mleibeng@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 09:10:14 by marvinleibe       #+#    #+#             */
-/*   Updated: 2024/06/07 07:40:14 by mleibeng         ###   ########.fr       */
+/*   Updated: 2024/06/07 08:01:17 by mleibeng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -380,7 +380,7 @@ int	check_column_bound(t_app *app)
 		j++;
 	}
 	j = 0;
-	while ( j < app->cols)
+	while (j < app->cols)
 	{
 		if (app->walked_map[app->rows - 1][j] == 2)
 		{
@@ -398,7 +398,7 @@ int	check_row_bound(t_app *app)
 	int	i;
 
 	i = 0;
-	while(i < app->rows)
+	while (i < app->rows)
 	{
 		if (app->walked_map[i][0] == 2)
 		{
@@ -409,7 +409,7 @@ int	check_row_bound(t_app *app)
 		i++;
 	}
 	i = 0;
-	while( i < app->rows)
+	while (i < app->rows)
 	{
 		if (app->walked_map[i][app->cols - 1] == 2)
 		{
@@ -425,9 +425,9 @@ int	check_row_bound(t_app *app)
 int	check_bounds(t_app *app)
 {
 	if (!check_column_bound(app))
-		return(0);
+		return (0);
 	if (!check_row_bound(app))
-		return(0);
+		return (0);
 	return (1);
 }
 
@@ -526,6 +526,53 @@ char	**map_validate(t_app *app, char *file)
 	return (map);
 }
 
+void	print_info(t_app *app)
+{
+	// Print the map
+	print_map(app->map);
+	print_walkedmap(app->walked_map, app->rows, app->cols);
+	// Print the mlx and image information
+	printf("mlx: %p\n", app->mlx);
+	printf("img: %p\n", app->img);
+	printf("compass: %p\n", app->compass);
+	printf("img width: %d\n", app->img->width);
+	printf("img height: %d\n", app->img->height);
+	// Print the texture information
+	printf("textures: %p\n", app->textures);
+	printf("texture NO: %s\n", app->textures->n_text);
+	printf("texture SO: %s\n", app->textures->s_text);
+	printf("texture WE: %s\n", app->textures->w_text);
+	printf("texture EA: %s\n", app->textures->e_text);
+	printf("texture floor: (%d, %d, %d)\n", app->textures->floor[0],
+		app->textures->floor[1], app->textures->floor[2]);
+	printf("texture ceiling: (%d, %d, %d)\n", app->textures->skybox[0],
+		app->textures->skybox[1], app->textures->skybox[2]);
+	// Print the player information
+	printf("player start x: %d\n", app->player.start_x);
+	printf("player start y: %d\n", app->player.start_y);
+	printf("player x: %.2f\n", app->player.x);
+	printf("player y: %.2f\n", app->player.y);
+	printf("player std_x: %.2f\n", app->player.std_x);
+	printf("player std_y: %.2f\n", app->player.std_y);
+	printf("player std_angle: %.2f\n", app->player.std_angle);
+	printf("player angle: %.2f\n", app->player.angle);
+	// Print the window and map information
+	printf("window width: %d\n", app->window_width);
+	printf("window height: %d\n", app->window_height);
+	printf("map height: %d\n", app->map_height);
+	printf("map width: %d\n", app->map_width);
+	// Print the FOV and ray information
+	printf("FOV: %.2f\n", app->fov);
+	printf("num rays: %d\n", app->num_rays);
+	printf("cur ray: %d\n", app->cur_ray);
+	// Print the check queue and walked map information
+	printf("check queue: %p\n", app->check_queue);
+	printf("cols: %d\n", app->cols);
+	printf("rows: %d\n", app->rows);
+	printf("end: %d\n", app->end);
+	printf("start: %d\n", app->start);
+}
+
 int	main(int argc, char **argv)
 {
 	t_app	app;
@@ -536,6 +583,7 @@ int	main(int argc, char **argv)
 		return (1);
 	if (_init_app(&app))
 		return (1);
+	print_info(&app);
 	mlx_image_to_window(app.mlx, app.compass, app.window_width - COMPASS_SIZE
 		- 10, 10);
 	app.compass->instances->z = 1;
