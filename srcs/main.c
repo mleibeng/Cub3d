fe9f4105ec6074ba06c74e21ca6525ee8940b628
Florian Keitel <fkeitel@2-G-1.42heilbronn.de>
2024-06-07 08:48:56 +0200

    main setup

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: fkeitel <fkeitel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 09:10:14 by marvinleibe       #+#    #+#             */
-/*   Updated: 2024/06/06 21:07:22 by fkeitel          ###   ########.fr       */
+/*   Updated: 2024/06/07 08:48:11 by fkeitel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,6 @@ int	main(void)
 		return (1);
 	}
 	ft_printf("MLX initialized successfully.\n");
-
 	app.img = mlx_new_image(app.mlx, app.window_width, app.window_height);
 	if (!app.img)
 	{
